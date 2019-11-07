698cf61c01ca513c32cc5dc2c42178719fc8f411
zamanOnline <zaman7.info@gmail.com>
2019-11-07 11:11:26 +0600

    create multi_images migration file

---
+++ database/migrations/2019_05_30_200929_create_multi_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMultiImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('multi_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('product_id');
+            $table->string('product_image');
+            $table->timestamps();
+            //$table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('multi_images');
+    }
+}
