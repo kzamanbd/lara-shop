475b2654545853e893a4500b14a3f9c1d883c40e
zamanOnline <zaman7.info@gmail.com>
2019-11-10 04:19:58 +0600

    create districts table migration file

---
+++ database/migrations/2019_11_09_204111_create_districts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedTinyInteger('division_id');
+            $table->string('name');
+            $table->string('bn_name');
+            $table->string('lat');
+            $table->string('lon');
+            $table->string('website');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+    }
+}
