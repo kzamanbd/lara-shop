be9bc4769a83e9af5c3ce0c4974c41c9b301c72b
zamanOnline <zaman7.info@gmail.com>
2019-12-02 20:15:10 +0600

    Update Multiple Product Images

---
+++ database/migrations/2019_05_30_200929_create_product_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedTinyInteger('product_id');
+            $table->string('image');
+            $table->string('path')->nullable();
+            $table->timestamps();
+            //$table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('multi_images');
+    }
+}
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		
-		<title>@yield('title') :: Electro</title>
+		<title>@yield('title') :: EshopBd</title>
 		<link rel="icon" type="text/css" href="{{ asset('frontend/favicon.png') }}">
 		<!-- Google font -->
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
